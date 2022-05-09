9348c78b8ff1b32dd115ab88e851e93ec2f6129f
Vinicius Pinheiro <viniciuspinheiro-_@hotmail.com>
2022-05-09 12:17:35 -0300

    Inicio projeto pagina de login

--- pizzaria/web-app/src/app/app.module.ts
+++ pizzaria/web-app/src/app/app.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Input, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { Ripple, RippleModule } from 'primeng/ripple';
+import { LoginComponent } from './login/login.component';
+import {InputTextModule} from 'primeng/inputtext';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CardModule,
-    ButtonModule
+    ButtonModule,
+    RippleModule,
+    InputTextModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
