470cbc1512d343495fa09b7c628e54b0286bc2a4
GuilhermeFontanella <gui.jmf@hotmail.com>
2022-06-10 14:30:58 -0300

    merge

--- pizzaria/web-app/src/app/app.module.ts
+++ pizzaria/web-app/src/app/app.module.ts
@@ -1,21 +1,45 @@
-import { NgModule } from '@angular/core';
+
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { Input, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-
+import { Ripple, RippleModule } from 'primeng/ripple';
+import { LoginComponent } from './login/login.component';
+import {InputTextModule} from 'primeng/inputtext';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HttpClientModule } from '@angular/common/http';
+import {PasswordModule} from 'primeng/password';
+import {InputMaskModule} from 'primeng/inputmask';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MenubarModule} from 'primeng/menubar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CardModule,
-    ButtonModule
+    ButtonModule,
+    RippleModule,
+    InputTextModule,
+    RouterModule,
+    HttpClientModule,
+    PasswordModule,
+    InputMaskModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
